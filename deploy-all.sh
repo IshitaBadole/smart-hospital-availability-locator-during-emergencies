f28c8514447c81d7fe533307ba32810d7a5835e8
Ishita Badole <ishitabadole@Ishitas-MacBook-Air.local>
2024-12-08 15:55:31 -0700

    add deploy shell scripts

---
+++ deploy-all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install -f minio/minio-config.yaml -n minio-ns --create-namespace minio-proj bitnami/minio
+
+kubectl apply -f redis/redis-deployment.yaml
+kubectl apply -f redis/redis-service.yaml
+
+kubectl apply -f rest/rest-deployment.yaml
+kubectl apply -f rest/rest-service.yaml
+
+kubectl apply -f logs/logs-deployment.yaml
+
+kubectl apply -f worker/worker-deployment.yaml
+
+kubectl apply -f minio/minio-deployment.yaml
+kubectl apply -f minio/minio-external-service.yaml
