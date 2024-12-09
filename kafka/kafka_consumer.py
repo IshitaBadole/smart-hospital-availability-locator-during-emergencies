0e57f998ebcfb6700de67882992fd1446770b66a
Anirudh Ragam <anirudhragam@Anirudhs-MacBook-Air.local>
2024-12-09 12:01:21 -0700

    add incremental learning for XGBRegressor in Kafka consumer

--- kafka/kafka_consumer.py
+++ kafka/kafka_consumer.py
@@ -29,17 +29,17 @@ def execute_with_retry(command, *args, **kwargs):
         # Creating the Redis client again
         redisClient = redis.StrictRedis(host=redisHost, 
                                 port=redisPort, 
-                                db=0)
+                                db=0, decode_responses=True)
         result = command(*args, **kwargs)  
         print(f"Command:{command} result:{result}")
         return result
 
-# Initializing minio variables
+# Defining minio variables
 minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
 minioUser = os.getenv("MINIO_USER") or "rootuser"
 minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
 
-# Defining Redis Variables
+# Defining Redis variables
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 redisPort = os.getenv("REDIS_PORT") or 6379
 
@@ -52,9 +52,17 @@ minio_client = Minio(minioHost,
 # Creating the Redis client
 redisClient = redis.StrictRedis(host=redisHost, 
                                     port=redisPort, 
-                                    db=0)
+                                    db=0, decode_responses=True)
 
+# Model variables
 bucket = "models"
+curr_model_version = 0
+
+# Initialzing an empty df to store incoming real-time samples
+# Retrain the XGB Regressor every 1000 real-time samples
+buffer = pd.DataFrame()
+buffer_size = 15
+previous_sample = {}
 
 # Creating the Kafka Client object and the Kafka topic
 client = KafkaClient("localhost:9092")
@@ -75,7 +83,7 @@ consumer = topic.get_simple_consumer(consumer_group=b"default",
 model = xgb.XGBRegressor()
 
 # Getting the V0 XGB model from Minio object storage
-response = minio_client.fget_object(bucket, 'xgb_model_v0.json', os.getcwd())
+response = minio_client.fget_object(bucket, 'xgb_model_v0.json', os.path.join(os.getcwd(),'xgb_model_v0.json'))
 
 # Loading the model
 model.load_model(os.path.join(os.getcwd(), 'xgb_model_v0.json'))
@@ -94,11 +102,13 @@ for msg in consumer:
     # the key does not exist which means that the hospital data has never been seen before
     if not key_exists:
         # initialize the lag features to 0
-        lags = [0] * 12
+        lags = [0.0] * 12
+        execute_with_retry(redisClient.rpush, ground_truth_key, *lags )
     else:
         # get the ground truth values stored in redis
         lags = redisClient.lrange(ground_truth_key, 0, -1)
         lags = execute_with_retry(redisClient.lrange, ground_truth_key, 0 , -1)
+        lags = [float(lag) for lag in lags]
 
     for i in range(12):
         sample[f"lag_{i+1}"] = lags[i]
@@ -113,7 +123,7 @@ for msg in consumer:
     # Storing the latitude, longitude, city and address of the current message before dropping them
     # from the sample. These variables are not required for prediction
     lat, lng = sample['lat'], sample['lng']
-    city, address = sample['city'], sample['address']
+    city, address, hospital_name = sample['city'], sample['address'], sample['hospital_name']
 
     # Deleting the columns not required for model predictions
     for key in ["lat", "lng", "city", "address", "timestamp", "subtotal_acute_utilization"]:
@@ -124,21 +134,65 @@ for msg in consumer:
         if col in mappings:
             sample[col] = mappings[col][sample[col]]
 
-    print(sample)
+    if hospital_name not in previous_sample:
+        previous_sample[hospital_name] = None
 
+    # Align target with the previous sample for this hospital
+    if previous_sample[hospital_name]:
+        # Set the target for the last sample as the current `simulated_utilization`
+        previous_sample[hospital_name]['target'] = sample['simulated_utilization']
+
+        # Add the previous sample to the buffer
+        prev_sample = pd.DataFrame(previous_sample[hospital_name], index = ['index'])
+        buffer = pd.concat([buffer, prev_sample], axis = 0)
+
+    # Update the last sample for this hospital
+    previous_sample[hospital_name] = sample.copy()
+
+    # If buffer limit has been reached, retrain the model, clear the buffer and
+    # store retrained model in the MinIO object storage
+    if len(buffer) == buffer_size:
+        
+        print("Retraining the XGBRegressor Model")
+
+        # Convert the buffer to a DataFrame
+        train_data = buffer.copy()
+        train_data.set_index(['index'], inplace=True)
+
+        # Separate features and target variable
+        X_train = train_data.drop(columns=['target'])
+        y_train = train_data['target']
+
+        # Convert data to DMatrix
+        dtrain = xgb.DMatrix(X_train, label=y_train)
+
+        # Update the model incrementally
+        model_booster = model.get_booster()
+        model_booster.update(dtrain, iteration=1)
+
+        # Store the retrained model in MinIO object storage
+        curr_model_version += 1
+        model_filename = f"xgb_model_v{curr_model_version}.json"
+        model_booster.save_model(model_filename)
+        minio_client.fput_object(bucket, model_filename, model_filename)
+
+        # Clearing the buffer
+        buffer = pd.DataFrame()
+        
     # Creating a dataframe of the current sample
     sample_df = pd.DataFrame(sample, index=['index'])
     sample_df.set_index(['index'], inplace=True)
-    print(sample_df.columns)
+    print(sample_df.dtypes)
 
     # Predict the utilization for the next hour
-    predicted_utilization = model.predict(sample_df)
+    predicted_utilization = float(model.predict(sample_df)[0])
+
 
     print("Predicted utilization: ", predicted_utilization)
     print()
 
     # Store the predicted utilization in Redis cache
-    predicted_utilization_key = f"pred_utilization:{sample['hospital_name']}"
+    predicted_utilization_key = f"pred_utilization:{hospital_name}"
     execute_with_retry(redisClient.set, predicted_utilization_key, predicted_utilization)
 
     # consumer.commit_offsets()
@@ -147,15 +201,3 @@ for msg in consumer:
     execute_with_retry(redisClient.rpush, ground_truth_key, sample['simulated_utilization'])
     # Remove the oldest lag
     execute_with_retry(redisClient.lpop, ground_truth_key)
-
-    # TODO: Store the current model in MinIO
-
-
-
-    
-
-
-
-
-    
-
