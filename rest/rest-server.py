78f82e0ce5da4ded52f6dff915aeb818eeb30c97
Anirudh Ragam <anirudhragam@Anirudhs-MacBook-Air.local>
2024-12-09 19:32:54 -0700

    Add worker-server and rest-server

--- rest/rest-server.py
+++ rest/rest-server.py
@@ -1,7 +1,6 @@
 # Imports
 
 from flask import Flask, request, Response, send_file
-import jsonpickle
 import base64
 import io
 import json
@@ -14,68 +13,37 @@ import redis
 # Initialize the Flask application
 app = Flask(__name__)
 
-# Defining Minio variables
-minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
-minioUser = os.getenv("MINIO_USER") or "rootuser"
-minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
-
 # Defining Redis Variables
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 redisPort = os.getenv("REDIS_PORT") or 6379
-
-
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/apiv1/separate', methods=['POST'])
-def separate():
-   
-    # Getting the ecoded MP3 from the JSON payload
-    data = jsonpickle.decode(request.data)
-    encoded_mp3 = data["mp3"]
- 
-    # Decoding the encoded MP3
-    decoded_mp3 = base64.b64decode(encoded_mp3)
-
-    # Creating a hash for the song
-    songhash = str(uuid.uuid1())
-
-    # Filename to be used when storing the mp3 on Minio
-    filename = f"{songhash}.mp3"
-    # Name onf the bucket to store the MP3 in
-    bucketname = "songs"
-
-    # Creating the Minio client
-    minioClient = Minio(minioHost,
-               secure=False,
-               access_key=minioUser,
-               secret_key=minioPasswd)
+@app.route('/api/v1/query', methods=['POST'])
+def query():
     
-    # Creating the Redis client
+    data = request.json
+
+    # Converting the payload to JSON format
+    json_data = json.loads(data)
+
+    # Getting the hospital_name from the JSON payload
+    hospital_name = json_data['hospital_name']
+
+    # Creating the Redis client to connect to the redis worker queue
     redisClient = redis.StrictRedis(host=redisHost, 
                                     port=redisPort, 
-                                    db=0)
+                                    db=1)
     
     # Pusing the songhash to be processed to the Redis Queue
-    redisClient.rpush("toWorker", songhash)
-
-    # If the "songs" bucket does not exist, create it
-    if not minioClient.bucket_exists(bucketname):
-        minioClient.make_bucket(bucketname)
-    
-    try:
-        # Storing the MP3 file in the dongs bucket
-        result = minioClient.put_object(bucketname, filename, io.BytesIO(decoded_mp3), length = len(decoded_mp3))
-    except InvalidResponseError as err:
-        print("Error when adding files the first time")
-        print(err)
+    redisClient.rpush("toWorker", hospital_name)
 
     # Returing a response after successfully storing the mp3 on minio and adding the song to the Redis Queu
     return {
-        "hash": songhash, 
-        "reason": "Song enqueued for separation"
+        "hospital_name": hospital_name, 
+        "reason": f"{hospital_name} added to queue"
     }
 
 
@@ -84,7 +52,7 @@ def get_queue():
     # Creating the Redis client
     redisClient = redis.StrictRedis(host=redisHost, 
                                     port=redisPort, 
-                                    db=0)
+                                    db=1)
     queue = []
     
     # Get all elements from the Redis list with the toWorker key
@@ -99,49 +67,5 @@ def get_queue():
         "queue": queue
     }
 
-@app.route("/apiv1/track/<songhash>/<track>", methods = ['GET'])
-def get_track(songhash, track):
-
-    # Creating the Minio client
-    minioClient = Minio(minioHost,
-               secure=False,
-               access_key=minioUser,
-               secret_key=minioPasswd)
-
-    # Getting the songhash-track.mp3 file from the output bucket on Minio
-    response = minioClient.get_object("output", f"{songhash}-{track}.mp3")
-
-    # return Response(
-        #     response.data,
-        #     mimetype='audio/mpeg',
-        #     headers={
-        #         'Content-Disposition': f'attachment; filename="{songhash}-{track}'
-        #     }
-        # )
-    return send_file(
-        io.BytesIO(response.data),
-        mimetype='audio/mpeg',
-        as_attachment=True,
-        download_name=f"{songhash}-{track}.mp3"
-    )
-  
-@app.route("/apiv1/remove/<songhash>/<track>", methods = ['DELETE'])
-def delete_track(songhash, track):
-
-    # Creating the Minio client
-    minioClient = Minio(minioHost,
-               secure=False,
-               access_key=minioUser,
-               secret_key=minioPasswd)
-    
-    # Removing the object from Minio
-    minioClient.remove_object("output",f"{songhash}-{track}.mp3")
-
-    return {
-        "track": f"{songhash}-{track}.mp3", 
-        "reason": f"{songhash}-{track}.mp3 has been deleted"
-    }
-
-    
 # start flask app
 app.run(host="0.0.0.0", port=5000)
--- worker/worker-server.py
+++ worker/worker-server.py
@@ -4,94 +4,43 @@ import sys
 import os
 import redis
 from minio import Minio, InvalidResponseError
+from geopy.geocoders import Nominatim
 
-# Defining Minio variables
-minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
-minioUser = os.getenv("MINIO_USER") or "rootuser"
-minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
 
 # Defining Redis Variables
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 redisPort = os.getenv("REDIS_PORT") or 6379
 
+# Defining the geolocator object
+geolocator = Nominatim(user_agent="my app")
 
-
-def separate_song_into_tracks(songhash):
-    """Function that accepts the songhash, separates into tracks using demucs and dumps the tracks back into the output bucket in minio"""
-
-    # Creating the Minio client object
-    minioClient = Minio(minioHost,
-               secure=False,
-               access_key=minioUser,
-               secret_key=minioPasswd)
-    
-    # Defining directories
-    input_dir = os.path.join(os.getcwd(),"data", "input")
-    output_dir = os.path.join(os.getcwd(),"data", "output")
-    models_dir = os.path.join(os.getcwd(),"data", "models")
-    
-    # Check if the output bucket in minio is already created
-    # If the "output" bucket does not exist, create it
-    if not minioClient.bucket_exists("output"):
-        print("Creating the output bucket on minio")
-        minioClient.make_bucket("output")
-
-    # Fetch the song from the songs bucket from minio
-    file_path = os.path.join(input_dir, f"{songhash}.mp3")
-    minioClient.fget_object("songs", f"{songhash}.mp3", file_path)
-    print(f"Downloaded {songhash}.mp3 to {file_path}")
-
-    # Defining the demucs command to separate the mp3 into tracks
-    demucs_cmd = f"python3 -m demucs.separate --out /srv/data/output /srv/data/input/{songhash}.mp3 --mp3"
-    
-    # Running the demucs command
-    print(demucs_cmd)
-    os.system(demucs_cmd)
-
-    # Fetch separated tracks from the output directory and put them into the output bucket in minio
-
-    # Defining directory path where the track files will be stored by demucs
-    output_track_dir = os.path.join(output_dir, "mdx_extra_q", songhash)
-    
-    # If the output directory exists
-    if os.path.isdir(output_track_dir):
-        # Iterating through all the track files
-        for root, _, files in os.walk(output_track_dir):
-            for file in files:
-                # Defining the complete track file path
-                track_file_path = os.path.join(output_track_dir, file)
-                # Getting the type of track (bass, drum, vocals, etc) to append in the Minio object name
-                track, _ = os.path.splitext(file)
-                # Defining the name of the minio object to be uploaded
-                minio_file_name = f"{songhash}-{track}.mp3"
-
-                # Uploading the track to the output bucket in Minio
-                minioClient.fput_object('output', minio_file_name, track_file_path)
-                print(f"Uploaded {songhash}-{track}.mp3 to output bucket")
-
-
-    # Remove songhash.mp3 from the songs bucket on minio after successfully separating it into tracks
-    minioClient.remove_object("songs",f"{songhash}.mp3")
-
-    
 if __name__ == '__main__':
 
     while True:
         try:
 
-            # Creating a redis client object
-            redisClient = redis.StrictRedis(host=redisHost, port=redisPort, db=0)
+            # Creating a redis client object for work queue
+            redis_queue = redis.StrictRedis(host=redisHost, port=redisPort, db=1)
+
+            # Creating a redis client object for the redis cache containing cached predictions
+            redis_cache = redis.StrictRedis(host=redisHost, port=redisPort, db=0)
+
             # BLPOP will block until an item is available in the list
             # Checking for items with the toWorker queue in the redis kist
-            work = redisClient.blpop("toWorker", timeout=0)
+            work = redis_queue.blpop("toWorker", timeout=0)
 
-            # Fetching the songhash from the item which will be at index 1
-            songhash = work[1].decode('utf-8')
-            print(songhash)
+            # Fetching the queried hospital name from the item which will be at index 1
+            hospital_name = work[1].decode('utf-8')
+            
+            # TODO: In the case of multiple matches for location add some 
+            # further logic to narrow down to the correct one?
+            location = geolocator.geocode(hospital_name , exactly_one=True)
 
-            # Separate mp3 into tracks and upload to Minio
-            result = separate_song_into_tracks(songhash)
+            # Getting the cached prediction for the queried hospital name
+            predicted_utilization_key = f"pred_utilization:{hospital_name}"
+            predicted_utilization = float(redis_cache.get(predicted_utilization_key))
 
+            print(f"Forecasted utilization for {hospital_name} is {predicted_utilization}")
 
         except Exception as exp:
             print(f"Exception raised in log loop: {str(exp)}")
