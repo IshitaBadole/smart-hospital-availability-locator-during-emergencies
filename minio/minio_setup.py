9f93e15081aa4101d01f5c9c1015ebab17223370
Anirudh Ragam <anirudhragam@Anirudhs-MacBook-Air.local>
2024-12-11 18:32:07 -0700

    Moving data to Minio

--- minio/minio_setup.py
+++ minio/minio_setup.py
@@ -7,37 +7,39 @@ minioHost = os.getenv("MINIO_HOST") or "localhost:9000"
 minioUser = os.getenv("MINIO_USER") or "rootuser"
 minioPasswd = os.getenv("MINIO_PASSWD") or "rootpass123"
 
-client = Minio(minioHost,
+def put_object(bucketname, bucket_filename, local_filepath, content_type="application/octet-stream"):
+
+    client = Minio(minioHost,
                secure=False,
                access_key=minioUser,
                secret_key=minioPasswd)
 
-bucketname='models'
+    if not client.bucket_exists(bucketname):
+        print(f"Create bucket {bucketname}")
+        client.make_bucket(bucketname)
 
-if not client.bucket_exists(bucketname):
-    print(f"Create bucket {bucketname}")
-    client.make_bucket(bucketname)
+    try:
+        objects = client.list_objects(bucketname)
+        is_empty = not any(objects)
 
-buckets = client.list_buckets()
+        if is_empty:
+            print(f"Bucket {bucketname} is empty. Add file {bucket_filename}")
+            client.fput_object(bucketname, 
+                               bucket_filename, 
+                               local_filepath,
+                               content_type=content_type)
+        else:
+            print(f"Objects in {bucketname} are:")
+            for obj in objects:
+                print(obj.object_name)
+    except Exception as e:
+        print("Error when adding files the first time")
+        print(str(e))
+            
+    print(f"Objects in {bucketname} are now:")
+    for thing in client.list_objects(bucketname, recursive=True):
+        print(thing.object_name)
 
-for bucket in buckets:
-    print(f"Bucket {bucket.name}, created {bucket.creation_date}")
-
-try:
-    objects = client.list_objects(bucketname)
-    is_empty = not any(objects)
-
-    if is_empty:
-        print(f"Bucket {bucketname} is empty. Add file xgb_model_v0.json")
-        client.fput_object(bucketname, "xgb_model_v0.json", os.path.join(os.getcwd(), "xgb_model.json"))
-    else:
-        print(f"Objects in {bucketname} are:")
-        for obj in objects:
-            print(obj.object_name)
-except Exception as e:
-    print("Error when adding files the first time")
-    print(str(e))
-        
-print(f"Objects in {bucketname} are now:")
-for thing in client.list_objects(bucketname, recursive=True):
-    print(thing.object_name)
+put_object("models", "xgb_model_v0.json", os.path.join(os.getcwd(), "models", "xgb_model.json"))
+put_object("data", "hospital_data.csv", os.path.join(os.getcwd(), "data", "final_hospital_data.csv"), content_type="application/csv")
+put_object("data", "mappings.json", os.path.join(os.getcwd(), "data", "mappings.json"), content_type="application/json")
